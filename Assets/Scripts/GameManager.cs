6987085208b953fb5d07956df96ba22b7a45f74b
Rozentor <rozentor2@gmail.com>
2020-11-30 21:03:58 +0500

    Added instantinating

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -64,6 +64,13 @@ public class GameManager : MonoBehaviourPunCallbacks
 
     #endregion
 
+    #region Public Fields
+
+    [Tooltip("The prefab to use for representing the player")]
+    public GameObject playerPrefab;
+
+    #endregion
+
     #region Private Methods
 
 
@@ -83,5 +90,16 @@ public class GameManager : MonoBehaviourPunCallbacks
     void Start()
     {
         Instance = this;
+
+        if (playerPrefab == null)
+        {
+            Debug.LogError("<Color=Red><a>Missing</a></Color> playerPrefab Reference. Please set it up in GameObject 'Game Manager'", this);
+        }
+        else
+        {
+            Debug.LogFormat("We are Instantiating LocalPlayer from {0}", Application.loadedLevelName);
+            // we're in a room. spawn a character for the local player. it gets synced by using PhotonNetwork.Instantiate
+            PhotonNetwork.Instantiate(playerPrefab.name, new Vector3(0f, 5f, 0f), Quaternion.identity);
+        }
     }
 }
